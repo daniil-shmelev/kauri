5e0c59105612278309d3a2e582024ebff9c52554
daniil-shmelev <164039932+daniil-shmelev@users.noreply.github.com>
2025-06-14 16:32:38 +0100

    Minor changes

--- kauri/maps.py
+++ kauri/maps.py
@@ -1,3 +1,18 @@
+# Copyright 2025 Daniil Shmelev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========================================================================
+
 """
 This module provides the :class:`Map` class, which implements linear multiplicative maps on trees
 and allows for their manipulation with respect to different Hopf algebras. In particular, this covers
